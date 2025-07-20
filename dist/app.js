8008a31cf590ecb0c8e781cba84637719318d0bd
zakarya eddoublali <zeddoublali@gmail.com>
2025-07-20 19:21:57 +0100

    add url fronet

--- dist/app.js
+++ dist/app.js
@@ -20,7 +20,7 @@ exports.prismaClient = (0, prismaMiddleware_1.setupPrismaMiddleware)(prismaBase)
 app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "uploads")));
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: "https://rentamanager.ma/", // Replace with your frontend URL
     credentials: true, // If you're using cookies or auth headers
 }));
 // root router
